b025aac4efad3646e41ef4de2840a8c3405815b0
pablo <paulrodmont@gmail.com>
2017-02-24 22:01:07 +0100

    PRimer commit

--- Aplicacion_Aprendizaje/Repositorio/RepositoryUsuarios.cs
+++ Aplicacion_Aprendizaje/Repositorio/RepositoryUsuarios.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Aplicacion_Aprendizaje.Models;
+using Aplicacion_Aprendizaje.Models.ViewModels;
 
 namespace Aplicacion_Aprendizaje.Repositorio
 {
-    public partial class RepositoryUsuarios : RepositoryBase<Usuarios>
+    public partial class RepositoryUsuarios : RepositoryBase<UsuariosViewModel>
     {
         public RepositoryUsuarios(UsuariosEntities context)
             : base(context)
